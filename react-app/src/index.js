472021be59dadb3670045c8f741b8accd46cb35b
vinayak0127 <vinayakrajbusiness@gmail.com>
2021-02-04 19:04:02 +0530

    day 2
    learned writing clean components,jsx css,jsx javascript,props
    working on mini book list projects

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -3,38 +3,37 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-function MyInfo() {
+const Title = 'Ikigai: The Japanese secret to a long and happy life'
+const Author = '~Héctor García'
+const img =' https://images-eu.ssl-images-amazon.com/images/I/81l3rZK4lnL._AC_UL200_SR200,200_.jpg'
+function BookList() {
   return (
-    <div>
-      <h1>
-        hwy this is Vinayak raj , software engineer
-      </h1>
-      <p>
-        undergraduate from india 
-      </p>
-      <h2>Skills i know</h2>
-      <ul>
-        <li>
-          c++
-        </li>
-        <li>
-          javascript
-        </li>
-        <li>
-          python
-        </li>
-      </ul>
-    </div>
+    <section className='booklist'>
+      <Book/>
+      <Book/>
+    </section>
+  )
+}
+
+const Book = (props) => {
+console.log(props)
+  return (
+    <article className='book'>
+        <img
+          src={img}
+          alt=''
+        />
+        <h1>{Title}</h1>
+        <h4>{Author}</h4>
+    </article>
   )
 }
 
 //jsx
-ReactDOM.render(
-  <MyInfo/>,//functional component
-  document.getElementById('root')
+ReactDOM.render( <BookList />,document.getElementById('root')
 );
 
 reportWebVitals();
-//26:min
 //freecodecamp
+//props
+//2:16:00
